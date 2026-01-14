e04d343df229f348383702b183d6c200a167614b
Zander Pantelick <zpantelick@gmail.com>
2026-01-14 14:35:14 -0800

    inclass 1/14/26

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe Ticket, type: :model do
     it "exists" do
         Ticket.new
     end
+
+    let (:ticket) { Ticket.new }
+
+    it "responds to name" do
+        expect(ticket).to respond_to(:name)
+        expect(ticket).to respond_to(:description)
+        expect(ticket).to respond_to(:phone)
+    end
+
+    it "belongs to region" do
+        should belong_to(:region)
+    end
+
+    # Do this for the optional ones
+    it "belongs to region" do
+        should belong_to(:region).optional
+    end
+
 end
