00b69f58e308a0808ef31a5c503666fd74ba6d67
Alec <aleckrsek@gmail.com>
2026-01-21 14:53:25 -0800

    Update ticket_spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -23,4 +23,35 @@ RSpec.describe Ticket, type: :model do
         should belong_to(:region).optional
     end
 
+    it "validates presence of required fields" do
+        should validate_presence_of(:name)
+        should validate_presence_of(:phone)
+        should validate_presence_of(:region_id)
+        should validate_presence_of(:resource_category_id)
+    end
+
+    it "has valide phone number" do
+        should validate_phony_plausible_of(:phone)
+    end
+
+    describe "to_s method" do
+        let (:ticket) { Ticket.new(id: 5) }
+        it "returns valid ticket id" do
+            expect(ticket.to_s).to eq("Ticket 5")
+        end
+    end
+
+    describe "scope tests" do
+        let (:ticket) { Ticket.new(organization: nil)}
+        let (:closed_ticket) { Ticket.new(closed: true) }
+
+        if "scopes open tickets" do
+            expect(Ticket.open).to include(ticket)
+        end
+
+        it "scopes closed tickets" do
+            expect(Ticket.closed).to include(closed_ticket)
+        end
+    end
+
 end
