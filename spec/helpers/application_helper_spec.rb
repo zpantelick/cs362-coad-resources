70bf2cc1eb27d8e46bcbcdb66243cabee3f65999
Alec Krsek <aleckrsek@gmail.com>
2026-01-26 15:17:50 -0800

    Add application helper title spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-
+    describe "test full title" do
+        it "works with no page title" do
+            expect(helper.full_title).to eq "Disaster Resource Network"
+        end
+        it "works with a page title" do
+            expect(helper.full_title("Contact")).to eq "Contact | Disaster Resource Network"
+        end
+    end
 end
