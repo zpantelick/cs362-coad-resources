cc35f677b69cfbf1076f9191c103b9fd9921dd3c
Zander Pantelick <zpantelick@gmail.com>
2026-02-09 15:09:07 -0800

    Updated Organization Spec to use factory and sequences

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,107 +1,115 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  it 'exists' do
-    Organization.new
-  end
-  describe "attribute tests" do
 
-    it 'has an email' do
-      expect(organization).to respond_to(:email)
+    setup do
+        @organization = create(:organization, :name => "Org Name")
     end
 
-    it 'has a name' do
-      expect(organization).to respond_to(:name)
+    it 'exists' do
+        Organization.new
     end
 
-    it 'has a phone' do
-      expect(organization).to respond_to(:phone)
+    describe "attribute tests" do
+
+        it 'has an email' do
+        expect(@organization).to respond_to(:email)
+        end
+
+        it 'has a name' do
+        expect(@organization).to respond_to(:name)
+        end
+
+        it 'has a phone' do
+        expect(@organization).to respond_to(:phone)
+        end
+
+        it 'has status' do
+        expect(@organization).to respond_to(:status)
+        end
+
+        it 'has a primary name' do
+        expect(@organization).to respond_to(:primary_name)
+        end
+
+        it 'has a secondary name' do
+        expect(@organization).to respond_to(:secondary_name)
+        end
+
+        it 'has a secondary phone' do
+        expect(@organization).to respond_to(:secondary_phone)
+        end
+
     end
 
-    it 'has status' do
-      expect(organization).to respond_to(:status)
+    describe "shoulda matchers" do
+
+        it 'has/belongs to a resource category' do
+            should have_and_belong_to_many(:resource_categories)
+        end
+
+        it "has many users" do
+            should have_many(:users)
+        end
+
+        it "has many tickets" do
+            should have_many(:tickets)
+        end
+
     end
 
-    it 'has a primary name' do
-      expect(organization).to respond_to(:primary_name)
+    describe "validations" do
+
+        it "validates length of email" do
+            should validate_length_of(:email)
+                .is_at_least(1)
+                .is_at_most(255)
+                .on(:create)
+        end
+
+        it "validates uniqueness of email" do
+            should validate_uniqueness_of(:email).case_insensitive
+        end
+
+        it "validates length of name" do
+            should validate_length_of(:name)
+                .is_at_least(1)
+                .is_at_most(255)
+                .on(:create)
+        end
+
+        it "validates uniqueness of name" do
+            should validate_uniqueness_of(:name).case_insensitive
+        end
+
+        it "validates length of description" do
+            should validate_length_of(:description)
+                .is_at_most(1020)
+                .on(:create)
+        end
     end
 
-    it 'has a secondary name' do
-      expect(organization).to respond_to(:secondary_name)
+    describe "to_s method" do 
+
+        it "returns valid name" do
+            expect(@organization.to_s).to eq('Org Name')
+        end
+
     end
 
-    it 'has a secondary phone' do
-      expect(organization).to respond_to(:secondary_phone)
+    describe "Class Tests" do
+
+        it "approve method works" do 
+                organization = Organization.new(:status => :rejected)
+                organization.approve()
+            expect(organization.status.to_sym).to eq(:approved)
+            end
+
+        it "reject method works" do 
+                organization = Organization.new(:status => :approved)
+                organization.reject()
+            expect(organization.status.to_sym).to eq(:rejected)
+            end
     end
-
-  end
-  let (:organization) { Organization.new }
-
-  
-
-  it 'has/belongs to a resource category' do
-    should have_and_belong_to_many(:resource_categories)
-  end
-
-  it "has many users" do
-    should have_many(:users)
-  end
-
-  it "has many tickets" do
-    should have_many(:tickets)
-  end
-
-  describe "validations" do
-    it "validates length of email" do
-      should validate_length_of(:email)
-        .is_at_least(1)
-        .is_at_most(255)
-        .on(:create)
-    end
-
-    it "validates uniqueness of email" do
-      should validate_uniqueness_of(:email).case_insensitive
-    end
-
-    it "validates length of name" do
-      should validate_length_of(:name)
-        .is_at_least(1)
-        .is_at_most(255)
-        .on(:create)
-    end
-
-    it "validates uniqueness of name" do
-      should validate_uniqueness_of(:name).case_insensitive
-    end
-
-    it "validates length of description" do
-      should validate_length_of(:description)
-        .is_at_most(1020)
-        .on(:create)
-    end
-  end
-
-  describe "to_s method" do 
-    let (:organization) { Organization.new(name: 'john doe') }
-    it "returns valid name" do
-      expect(organization.to_s).to eq('john doe')
-    end
-  end
-
-  describe "Class Tests" do
-    let (:organization) { Organization.new() }
-
-    it "approve method works" do 
-			organization = Organization.new(:status => :rejected)
-			organization.approve()
-      	expect(organization.status.to_sym).to eq(:approved)
-		end
-
-    it "reject method works" do 
-			organization = Organization.new(:status => :approved)
-			organization.reject()
-      	expect(organization.status.to_sym).to eq(:rejected)
-		end
-  end
 
 end
