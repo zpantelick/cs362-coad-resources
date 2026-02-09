8a7e2fd0b73e2cbad929b86b7b218f13b2be69e7
Alec Krsek <aleckrsek@gmail.com>
2026-02-09 14:45:48 -0800

    Add regions controller spec

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+    describe "as a logged-out user" do
+        let (:user) { create(:user) }
+        
+        it {
+            expect(get(:index)).to redirect_to(new_user_session_path)
+        }
+    end
 
+    describe "as a logged-in user" do
+        let (:user) { create(:user) }
+        before(:each) { sign_in user }
+
+        it {
+            expect(get(:index)).to redirect_to(dashboard_path)
+        }
+    end
+
+    describe "as an admin user" do
+        let (:user) { create(:user, :admin) }
+        before(:each) { sign_in user }
+
+        it {
+            expect(get(:index)).to be_successful
+        }
+    end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,14 @@
 FactoryBot.define do
 
     factory :user do
-        email { :email }
+        email { "user@example.com" }
+        password { "foopass123" }
         organization_id { }  # will need an org id, but that should probably be taken from an organization factory, so it references it correctly
                              # ie. ":organization_id" from spec/factories/organization.rb when that exists
         before(:create) { |user| user.skip_confirmation! }
 
+        trait :admin do
+            role { :admin }
+        end
     end
 end
