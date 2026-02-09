955cb59ed573c46a472e67ef4b17933bddb2534c
Alec Krsek <aleckrsek@gmail.com>
2026-02-09 14:38:20 -0800

    Add user skip confirmation

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
         email { :email }
         organization_id { }  # will need an org id, but that should probably be taken from an organization factory, so it references it correctly
                              # ie. ":organization_id" from spec/factories/organization.rb when that exists
+        before(:create) { |user| user.skip_confirmation! }
+
     end
 end
