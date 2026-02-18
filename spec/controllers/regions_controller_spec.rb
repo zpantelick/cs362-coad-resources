9a1480f78f7d23ca299fcb84ddd84be53bd35fcb
Alec Krsek <aleckrsek@gmail.com>
2026-02-17 20:36:39 -0800

    Finish regions controller

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+    shared_context "as admin" do
+        let (:user) { create(:user, :admin) }
+        before(:each) { sign_in user }
+    end
+
     describe "as a logged-out user" do
         let (:user) { create(:user) }
         
@@ -27,11 +32,11 @@ RSpec.describe RegionsController, type: :controller do
             post(:create, params: { region: FactoryBot.attributes_for(:region) })
             expect(response).to redirect_to dashboard_path
         }
+        
     end
 
     describe "as an admin user" do
-        let (:user) { create(:user, :admin) }
-        before(:each) { sign_in user }
+        include_context "as admin"
 
         it {
             expect(get(:index)).to be_successful
@@ -41,5 +46,74 @@ RSpec.describe RegionsController, type: :controller do
             post(:create, params: { region: FactoryBot.attributes_for(:region) })
             expect(response).to redirect_to regions_path
         }
+
+        it {
+            post(:create, params: { region: FactoryBot.attributes_for(:region, name: nil) })
+            expect(response).to have_http_status(:ok)
+            expect(response).not_to redirect_to regions_path
+        }
     end
+
+    describe "show method" do
+        let (:region) { create(:region) }
+        include_context "as admin"
+
+        it {
+            get(:show, params: { id: region.id })
+            expect(response).to be_successful
+        }
+    end
+
+    describe "new method" do
+        include_context "as admin"
+
+        it {
+            get(:new)
+            expect(response).to be_successful
+        }
+    end
+
+    describe "edit method" do
+        let (:region) { create(:region) }
+        include_context "as admin"
+
+        it {
+            get(:edit, params: { id: region.id })
+            expect(response).to be_successful
+        }
+    end
+
+    describe "update method" do
+        context "with valid attributes" do
+            let (:region) { create(:region) }
+            include_context "as admin"
+
+            it {
+                patch(:update, params: { id: region.id, region: FactoryBot.attributes_for(:region) })
+                expect(response).to redirect_to region_path(region)
+            }
+        end
+
+        context "with invalid attributes" do
+            let (:region) { create(:region) }
+            include_context "as admin"
+
+            it {
+                patch(:update, params: { id: region.id, region: FactoryBot.attributes_for(:region, name: nil) })
+                expect(response).to have_http_status(:ok)
+                expect(response).not_to redirect_to(region_path(region))
+            }
+        end
+    end
+
+    describe "destroy method" do
+        let (:region) { create(:region) }
+        include_context "as admin"
+
+        it {
+            delete(:destroy, params: { id: region.id })
+            expect(response).to redirect_to regions_path
+        }
+    end
+
 end
